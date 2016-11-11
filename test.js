3290176920b3ac4bedd033185a5bf992a45ffa91
Kevin McGowan <mcgowan.kevin@googlemail.com>
2016-11-11 21:35:48 +0000

    PokemonGO: Version 0.45.0 update

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var index = require("./index.js");
 var assert = require("assert");
 
 /// SET YOUR HASHING SERVER
-// index.utils.useHashingServer("your ip", 1500);
+index.utils.useHashingServer("YOUR_SERVER_IP", 1501);
 
 /// Simple test to ensure that responses are correct from each value
 
@@ -10,35 +10,35 @@ index.utils.hashLocation2(0,0,0,true).then((res) => {
     const debug_payload = res[1];
     res = res[0];
     console.log(`0,0,0 = ${res} | hi: ${debug_payload.high.toString(16)} lo: ${debug_payload.low.toString(16)} | 0x${debug_payload.high.toString(16)}${debug_payload.low.toString(16)}`);
-    assert(res === 3937442440);
+    assert(res === 413437985);
 });
 
 index.utils.hashLocation2(50,10,20,true).then((res) => {
     const debug_payload = res[1];
     res = res[0];
     console.log(`50,10,20 = ${res} | hi: ${debug_payload.high.toString(16)} lo: ${debug_payload.low.toString(16)} | 0x${debug_payload.high.toString(16)}${debug_payload.low.toString(16)}`);
-    assert(res === 1063829902);
+    assert(res === 1533251323);
 });
 
 index.utils.hashLocation2(53,18,25,true).then((res) => {
     const debug_payload = res[1];
     res = res[0];
     console.log(`53,18,25 = ${res} | hi: ${debug_payload.high.toString(16)} lo: ${debug_payload.low.toString(16)} | 0x${debug_payload.high.toString(16)}${debug_payload.low.toString(16)}`);
-    assert(res === 1362775368);
+    assert(res === 336542163);
 });
 
 index.utils.hashLocation2(55,22,18,true).then((res) => {
     const debug_payload = res[1];
     res = res[0];
     console.log(`55,22,18 = ${res} | hi: ${debug_payload.high.toString(16)} lo: ${debug_payload.low.toString(16)} | 0x${debug_payload.high.toString(16)}${debug_payload.low.toString(16)}`);
-    assert(res === 4126390853);
+    assert(res === 1970450721);
 });
 
 index.utils.hashLocation2(42,-18,11,true).then((res) => {
     const debug_payload = res[1];
     res = res[0];
     console.log(`42,-18,11 = ${res} | hi: ${debug_payload.high.toString(16)} lo: ${debug_payload.low.toString(16)} | 0x${debug_payload.high.toString(16)}${debug_payload.low.toString(16)}`);
-    assert(res === 1567044474);
+    assert(res === 1477657678);
 });
 
 const test = new Buffer(24);
@@ -50,7 +50,7 @@ index.utils.hashLocation1(test, 0, 0, 0, true).then((res) => {
     const debug_payload = res[1];
     res = res[0];
     console.log(`all 0 = ${res} | hi: ${debug_payload.high.toString(16)} lo: ${debug_payload.low.toString(16)} | 0x${debug_payload.high.toString(16)}${debug_payload.low.toString(16)}`);
-    assert(res === 1345880194);
+    assert(res === 2756660921);
 });
 
 const bytes = new Buffer(32);
@@ -63,5 +63,5 @@ builder.setAuthTicket(test, true);
 builder.setLocation(0,0,0);
 builder.encrypt([test], (err, encryptedSig) => {
     console.log("Builder response " + encryptedSig.toString("hex"));
-    assert(encryptedSig.toString("hex") === "000001f4f709274ec09e849273e90957837ee4d0a06dbb6af9aae307535e94af95dfee5153b3664b8339334564e4fd6fcf7acbb599ddcaa29babaa8de7627161014507750ca57b897d07de8b780e555304cce4fa61e54d6ca3a6ef39317af0c611013cdc10f93ed1aeabbaad39682cf57b9c0146ba29424c11bc26cce0778bcdef50aa3e44599b2c20987d23b405e12d04e4b553026d1b3fd9e39d99c86716af880c6db557c1e85ed9db13fe71522da7f3bc2b9d8b0a0a39654e90d667de464a4fd07bca9aca910ac7b88e946d05db38d6276161dbbd8d6358fc9401cf2de012208a0ff6fa7d59f2095538cde6903576163d5ff57d95f441811bebd84cf0daa45d4402319a");
+    assert(encryptedSig.toString("hex") === "000001f47248f3056bc3458ffd082194926e7edd852df7e5705647f45b1d47551812dbd06d12bf91a82bc873e2fadd44445ff560a79ac498bd550aa3c473db12b5483225aa5e82d8e01de0fd7b26154c229de9296de9aee92aff20b81db87dbc2acba36c1628f9280b811e5404f06b8e5a15f3bfae9220f11dc9c74d51d62b20fc792dde61a9ed09ae785e50a470e34722771df222fb10445f8efef25ae2556e9e1713c2648c1211d036ce5040a0d89ad9cf86168c173b1b5e03542dc2806929e0450444b163c8027e99e35b78c30f3be31621ae59e75afe544a7e147e229f7ea89daf044a4826aaaaaea64c57065f552e315e566e0e8a3822ed62cf43e96078f8c009029a");
 });
